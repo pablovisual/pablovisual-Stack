edc5458a6272d322d73afe3e37e9b85965072f0a
pablovisual <pabloarmandosandoval@gmail.com>
2023-02-17 18:14:15 +0000

    lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,6 @@
 import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
 /**
  * Stacks are LIFO - last in, first out, like a stack of plates or books, or the puzzle game "Towers of Hanoi".
  * They are used in algorithms where it is needed to keep track of the ordering in which the elements were inserted.
@@ -20,7 +22,8 @@ public class Lab {
      * @return a newly instantiated Deque. Deques implement both stack and queue behavior.
      */
     public Deque<String> createNewStack(){
-        return null;
+        Deque<String> list = new LinkedList<>();
+        return list;
     }
 
     /**
@@ -28,7 +31,7 @@ public class Lab {
      * @return the number of elements in the deque.
      */
     public int getSize(Deque<String> stack){
-        return 0;
+        return stack.size();
     }
 
     /**
@@ -38,6 +41,7 @@ public class Lab {
      * @param item a String to be added to the deque.
      */
     public void addToTopOfStack(Deque<String> stack, String item){
+        stack.addFirst(item);
     }
 
     /**
@@ -47,7 +51,7 @@ public class Lab {
      * @return the value popped from the top of the deque.
      */
     public String removeFromTopOfStack(Deque<String> stack){
-        return "";
+        return stack.pop();
     }
     /**
      * Peek the value at the top of a deque.
@@ -57,6 +61,6 @@ public class Lab {
      * @return the value peeked from the top of the deque.
      */
     public String getTopOfStackWithoutRemoving(Deque<String> stack){
-        return "";
+        return stack.peek();
     }
 }
